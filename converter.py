5f496bd696341537447abfe66907959e13e56830
DESKTOP-HF35O0S\lukasz <lukasz.buturla.25@gmail.com>
2022-11-03 19:46:34 +0100

    Create function to get values from one subject

--- converter.py
+++ converter.py
@@ -4,5 +4,26 @@ class Converter:
         
         with open(pathToRead, "r", encoding="UTF-8") as fileRead:
 
-            lines = fileRead.readlines()
-            print(lines)
+            lines = fileRead.read()
+            
+            subjectStart = lines.find("<Pozycja>")
+            subjectEnd = lines.find("</Pozycja>")
+            subject = lines[subjectStart:subjectEnd]
+            
+            indexStart = subject.find("<Indeks>")
+            indexEnd = subject.find("</Indeks>")
+            index = subject[indexStart+8:indexEnd]
+            
+            amountStart = subject.find("<Ilosc>")
+            amountEnd = subject.find("</Ilosc>")
+            amount = subject[amountStart+7:amountEnd]
+            
+            priceStart = subject.find("<Cena>")
+            priceEnd = subject.find("</Cena>")
+            price = subject[priceStart+6:priceEnd]
+            
+            
+            print(index)
+            print(amount)
+            print(price)
+            

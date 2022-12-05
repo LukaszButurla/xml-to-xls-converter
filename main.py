b5b3a99a22610ec0f2bd1d6c3ff60c5ac1527b90
DESKTOP-HF35O0S\lukasz <lukasz.buturla.25@gmail.com>
2022-12-05 23:05:17 +0100

    Add value to datatable

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ class MainWindow(MDApp):
     def select_file_window(self, btn):
         self.selectedFile = filedialog.askopenfilename(filetypes=[("Text files", "xml")])
         self.labelSelectDirectory.text = self.selectedFile
+        self.datatableClass.add_values_from_file(self.selectedFile)
         
     def select_directory_to_save_window(self, btn):
         self.selectedDirectory = filedialog.askdirectory()
